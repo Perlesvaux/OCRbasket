5ee4a88f75b1ce7e4aed493d9399a804d9ff8243
chico <paltryhumanity@gmail.com>
2023-09-17 18:29:17 -0600

    simplified frompaste.html. Working on image lazy-loading

--- src/main/java/corbenic/basket/Basket.java
+++ src/main/java/corbenic/basket/Basket.java
@@ -1,50 +1,23 @@
 package corbenic.basket;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.beans.factory.annotation.Autowired;
-// import java.io.File;
-// import java.io.IOException;
-// import java.io.FileReader;
-// import java.io.BufferedReader;
-import java.io.*;
 import org.springframework.util.FileCopyUtils;
 import net.sourceforge.tess4j.TesseractException;
-// import org.springframework.web.bind.annotation.CrossOrigin;
+import java.io.*;
 
 @RestController
 public class Basket{
 
-    @Autowired
-    BasketService uploader;
-
-    // @CrossOrigin(origins = "http://localhost:8080")
     @PostMapping("/upload")
     public String uploadingFile(@RequestBody MultipartFile file) throws IllegalStateException, IOException, TesseractException{
 
-    //This one works great by binding it to a form named "file"
-    //public String uploadingFile(@RequestParam("file") MultipartFile file) throws IllegalStateException, IOException, TesseractException{
-
-
-        //This one works great with plain text files!
-        // byte[] fileBytes   = file.getBytes();
-        // String fileContent = new String(fileBytes, "UTF-8");
-        // return fileContent;
-
-
-        //This one should work for images:
         File temp =  File.createTempFile("temp", ".png"); //Just a convention. The extension doesn't matter.
         String result = "";
         try{
             FileCopyUtils.copy(file.getInputStream(), new FileOutputStream(temp));
-            result = Ocr.dump(
-                        Scaling.process(temp)
-                        // temp
-                    );
-            // result = Ocr.dump(temp);
-        // System.out.print(result);
+            result = Ocr.dump(Scaling.process(temp));
         return result;
         } catch(IOException e) {
             result =  "mission failed :p";
@@ -52,21 +25,6 @@ public class Basket{
         } finally {
             temp.delete();
         }
-
-
-
-
-
-
-
-        // try(BufferedReader fp = new BufferedReader(file.getInputStream())){
-
-        // }
-        // uploader.uploading(file);
-        // return "file: " + file.getOriginalFilename() + " has been uploaded :D!";
-
-
-
     }
 }
 
